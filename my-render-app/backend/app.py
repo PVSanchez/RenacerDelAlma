ce1b7ee2601fe12ba34b6357c684d6b6bc324dbb
drolo18 <pedro.velez.hlvt@gmail.com>
2025-07-28 21:57:33 +0200

    migrations realized

--- my-render-app/backend/app.py
+++ my-render-app/backend/app.py
@@ -1,6 +1,19 @@
+
 from flask import Flask, jsonify
+from flask_migrate import Migrate
+from database.db import db
+from models.User import User
+from models.Rol import Rol
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///database/app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Inicializa la base de datos y migraciones
+db.init_app(app)
+migrate = Migrate(app, db)
+
 
 @app.route('/api', methods=['GET'])
 def home():
